f6e0b85dc18a9f413d02504c52c9f460b96426be
Geoff.Schmid <Geoff.Schmid@BCFCWSMS05.bluechipdomain.co.uk>
2015-02-04 11:18:57 +0000

    ..

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 int32_t main(void) {
-    printf("Hola World from Eclipse to GitHub to Jenkins to Test Run\n"); 
+    printf("Hola World Seb from Eclipse to GitHub to Jenkins to Test Run\n"); 
     printf("Clean up 28/1/15\n");  
     printf("********************************************************\n");
     return 1;
